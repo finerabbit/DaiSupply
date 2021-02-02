bcb00caf9382c9123b1d184d294cdfb07a6889f8
Lucky Rabbit <luckyrabbit870930@gmail.com>
2021-02-03 04:56:47 +0900

    Success.

--- DaiSupply.js
+++ DaiSupply.js
@@ -1,5 +1,6 @@
 const Web3 = require('web3');
-const web3 = new Web3('https://mainnet.infura.io/v3/<INFURA_ID>');
+require('dotenv').config();
+const web3 = new Web3('https://mainnet.infura.io/v3/' + process.env.API_KEY);
 
 const vatAddr = '0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B';
 const abi = [{
@@ -10,6 +11,7 @@ const abi = [{
 }];
 
 const vat = new web3.eth.Contract(abi, vatAddr);
+
 vat.methods.debt().call().then(
   supply => console.log(supply/Math.pow(10,45))
 );
