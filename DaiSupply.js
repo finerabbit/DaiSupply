82a6074e9733d2efd14e7cabba8f2fc0bbf10878
unknown <luckyrabbit870930@gmail.com>
2021-02-09 06:22:45 +0900

    uniswap test.

--- DaiSupply.js
+++ DaiSupply.js
@@ -1,5 +1,6 @@
 const Web3 = require('web3');
 require('dotenv').config();
+const { ChainId, Token } = require('@uniswap/sdk');
 const web3 = new Web3('https://mainnet.infura.io/v3/' + process.env.API_KEY);
 
 const vatAddr = '0x35D1b3F3D7966A1DFe207aa4514C12a259A0492B';
@@ -28,3 +29,13 @@ const erc20dai = new web3.eth.Contract(ercabi, erc20daiAddr);
 erc20dai.methods.totalSupply().call().then(
   supply => console.log(supply/Math.pow(10,18))
 );
+
+//console.log(`The chainId of mainnet is ${UNISWAP.ChainId.MAINNET}.`);
+
+const chainId = ChainId.MAINNET;
+const tokenAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // must be checksummed
+const decimals = 18;
+
+const DAI = new Token(chainId, tokenAddress, decimals);
+
+console.log(chainId);
